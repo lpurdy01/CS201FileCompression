957d9afc53dfe05f88034b8dca1d658d552360c6
lpurdy01 <lpurdy01@gmail.com>
2017-11-29 21:46:17 -0900

    Made two player scoring system

--- Roll a Ball/Assets/Scripts/BallScore.cs
+++ Roll a Ball/Assets/Scripts/BallScore.cs
@@ -5,16 +5,26 @@ using UnityEngine.UI;
 
 public class BallScore : MonoBehaviour {
 
-    public Text countText;
+    public Text player1count;
+    public Text player2count;
     public Text winText;
-    public static int count;
+    public static int count1;
+    public static int count2;
+    public static int winnumber;
+
+    public Material player1Meterial;
+    public Material player2Meterial;
+    private int player;
 
 
     // Use this for initialization
     void Start () {
         SetCountText();
         winText.text = "";
-        count = 0;
+        count1 = 0;
+        count2 = 0;
+        player = 0;
+        winnumber = 5;
     }
 
     // Update is called once per frame
@@ -24,21 +34,61 @@ public class BallScore : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Collision was made");
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            GetComponent<Renderer>().material = player1Meterial;
+            player = 1;
+        }
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            GetComponent<Renderer>().material = player2Meterial;
+            player = 2;
+        }
         if (other.gameObject.CompareTag("Pick Up"))
         {
-            other.gameObject.SetActive(false);
-            count++;
+            if(player == 1)
+            {
+                count1++;
+                SetCountText();
+                Destroy(gameObject);
+            }
+            if (player == 2)
+            {
+                count2++;
+                SetCountText();
+                Destroy(gameObject);
+            }
+            if (player == 0)
+            {
+                transform.position = new Vector3(0, 1, 0);
+            }
+
             SetCountText();
         }
+        
     }
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count == 4)
+        player1count.text = "Player1 Score: " + count1.ToString();
+        player2count.text = "Player2 Score: " + count2.ToString();
+        if (count1+count2 == winnumber)
         {
-            winText.text = "You Win!";
-            //winText.enabled = true;
-        }
+            if(count1 > count2)
+            {
+                winText.text = "Player 1 Wins!";
+            }
+            else if (count2 > count1)
+            {
+                winText.text = "Player 2 Wins!";
+            }
+            else
+            {
+                winText.text = "You Win!";
+            }
+
+                //winText.enabled = true;
+            }
     }
 }
